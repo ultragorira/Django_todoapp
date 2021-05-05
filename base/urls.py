26abd7cdfe7334f39392149c83d620562cd5b32e
ultragorira <lorisdeluca1988@gmail.com>
2021-05-05 21:47:57 +0300

    Login view

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,6 @@ from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', SignupPage.as_view(), name='register'),
-
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
